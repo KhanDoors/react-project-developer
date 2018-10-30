01ef19cb2ebbd53d4b6ec863c8f151124bc0996c
unknown <syyadkhan@yahoo.com>
2018-10-29 20:08:38 -0400

    component tree and router setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
 
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Let's do it again!!</h1>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <h1>Let's do it again!!</h1>
+        </div>
+      </BrowserRouter>
     );
   }
 }
