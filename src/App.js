f774892a4ca2c5b21d858812b3b31a685329f12f
unknown <syyadkhan@yahoo.com>
2018-10-29 20:42:59 -0400

    navbar complete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
 import { BrowserRouter } from "react-router-dom";
-
-import "./App.css";
+import Navbar from "./components/layout/Navbar";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <h1>Let's do it again!!</h1>
+          <Navbar />
         </div>
       </BrowserRouter>
     );
