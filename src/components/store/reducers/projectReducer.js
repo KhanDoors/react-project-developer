4fcd529490f69430235d3648b85fbb2ae70dd7be
unknown <syyadkhan@yahoo.com>
2018-10-31 20:49:44 -0400

    redux setup

--- src/components/store/reducers/projectReducer.js
+++ src/components/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {};
+const initState = {
+  projects: [
+    { id: "1", title: "cow jumped over moon", content: " rrr ttt" },
+    { id: "2", title: "cat in the hat", content: "wwwwww" },
+    { id: "3", title: "mary had a lamb", content: "llllll" }
+  ]
+};
 
 const projectReducer = (state = initState, action) => {
   return state;
