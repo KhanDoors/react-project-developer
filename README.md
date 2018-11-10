ad6cf04f2f90879c843b7d22a06bf0487c9c87cc
unknown <syyadkhan@yahoo.com>
2018-11-10 11:11:38 -0500

    final Project complete and deployed to Firebase

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # react-project-developer
+
 This will be a React / Firestore project to design and store development projects.
+
+Link: https://project-developer-9843f.firebaseapp.com/signin
---
+++ dist/precache-manifest.d02b21853fa10574460988b0c70bdc52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5c1150b8e7ceac55f1ed",
+    "url": "/static/js/main.5c1150b8.chunk.js"
+  },
+  {
+    "revision": "f0697064650e69a4b2bc",
+    "url": "/static/js/1.f0697064.chunk.js"
+  },
+  {
+    "revision": "5c1150b8e7ceac55f1ed",
+    "url": "/static/css/main.f080f6d0.chunk.css"
+  },
+  {
+    "revision": "35ff5befdbe972cfa3339eb45893207f",
+    "url": "/index.html"
+  }
+];
